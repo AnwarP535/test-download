63672124b2a803ec49afb15175697b789701491d
Anwar Shad <95863015+AnwarP535@users.noreply.github.com>
2023-12-06 11:53:45 +0530

    added files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <a href="https://docs.google.com/document/export?format=docx&id=1Tb22Rlx3k6VxsoHF6yg_bxTKWi2H9H4T5yjTApl3wVk" download="required-docs">download</a>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
